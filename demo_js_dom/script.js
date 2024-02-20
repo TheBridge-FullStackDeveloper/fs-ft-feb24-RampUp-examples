a35e7c5f0222400a896f9f3515f02285d7586858
Alejandro Reyes <reyesbalejandroe@gmail.com>
2024-02-20 11:57:40 +0100

    add/dom_manipulation

--- demo_js_dom/script.js
+++ demo_js_dom/script.js
@@ -122,3 +122,105 @@ function sayHello() {
 document.querySelector("#mensaje").addEventListener("mouseout", sendAlert);
 
 document.querySelector("#mensaje").addEventListener("mouseover", sayHello);
+
+// Poner/quitar clase
+document.querySelector("#boton4").addEventListener("click", function () {
+  document.querySelector("#div1").classList.toggle("fondo");
+});
+
+document.querySelector("#texto").addEventListener("mouseover", function () {
+  document.querySelector("h3").classList.toggle("fondo");
+});
+
+document.querySelector("#texto").addEventListener("mouseout", function () {
+  document.querySelector("h3").classList.toggle("fondo");
+});
+
+document.getElementById("boton5").addEventListener("click",function(){
+  //document.getElementById("div2").innerHTML += "<p>queso</p>";
+  const parent = document.getElementById("div2");
+  const child = document.createElement("p");
+  const txt = document.createTextNode("Texto para párrafo desde JS");
+  
+  child.appendChild(txt); // <p>Texto para párrafo</p>
+  parent.appendChild(child);    //<div id="div2"><p>Texto para párrafo</p></div>
+
+
+  const child2 = document.createElement("p");
+  const txt2 = document.createTextNode("Texto 2"+Math.random());
+  child2.appendChild(txt2); 
+  child2.setAttribute("id","p1"); 
+
+  let oldChild = document.getElementById("p1");
+  console.log(oldChild);
+  parent.replaceChild(child2,oldChild)
+})
+
+document.getElementById("boton6").addEventListener("click",function(){
+    const datos = ["Patatas", "Pescado", "Naranja", "Queso"]; // viene de un "servidor externo"
+
+    let template = `<ul>
+                      <li>${datos[0]}</li>
+                      <li>${datos[1]}</li>
+                      <li>${datos[2]}</li>
+                      <li>${datos[3]}</li>
+                    </ul>`;
+
+    document.getElementById("div3").innerHTML=template;
+})
+
+document.getElementById("boton7").addEventListener("click", function () {
+  const datos = [
+    { marca: "BMW", peso: 1600, color: "rojo" },
+    { marca: "BMW", peso: 1600, color: "azul" },
+    { marca: "BMW", peso: 1600, color: "verde" },
+    { marca: "BMW", peso: 1600, color: "amarillo" },
+  ]; // viene de un "servidor externo"
+  // Template string
+  const lista = `<section>
+                  <article>
+                    <p>${datos[0].marca}</p>
+                    <p>${datos[0].peso}</p>
+                    <p>${datos[0].color}</p>
+                  </article>
+                  <article>
+                    <p>${datos[1].marca}</p>
+                    <p>${datos[1].peso}</p>
+                    <p>${datos[1].color}</p>
+                  </article>
+                  <article>
+                    <p>${datos[2].marca}</p>
+                    <p>${datos[2].peso}</p>
+                    <p>${datos[2].color}</p>
+                </article>
+                <article>
+                  <p>${datos[3].marca}</p>
+                  <p>${datos[3].peso}</p>
+                  <p>${datos[3].color}</p>
+              </article>
+
+                </section>`;
+  document.getElementById("lista2").innerHTML = lista;
+});
+
+
+document.getElementById("boton8").addEventListener("click", function () {
+  const datos = [
+    { marca: "BMW", peso: 1600, color: "rojo" },
+    { marca: "BMW", peso: 1600, color: "azul" },
+    { marca: "BMW", peso: 1600, color: "verde" },
+    { marca: "BMW", peso: 1600, color: "amarillo" },
+  ]; // viene de un "servidor externo"
+  // Template string
+  let lista = "<section>";
+
+  for (let i = 0; i < datos.length; i++) {
+    lista += `<article>
+              <p>${datos[i].marca}</p>
+              <p>${datos[i].peso}</p>
+              <p>${datos[i].color}</p>
+            </article>`
+  }
+    lista += "</section>";
+  document.getElementById("lista3").innerHTML = lista;
+});
